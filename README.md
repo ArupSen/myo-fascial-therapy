98242a0c7286fdac9507ee071a959325a8bd6a61
Arup Sen <arupsen48@gmail.com>
2015-12-07 16:04:27 +0000

    Add egoscue page with 2 images

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ myo-fascial-therapy
 ===================
 
 Myo Fascial Therapy website for Astrid Spoon
+The url is [http://myofascial-therapy.co.uk/](http://myofascial-therapy.co.uk/)
---
+++ egoscue.php
@@ -0,0 +1,38 @@
+<?php
+
+// -- egoscue.php --
+require_once 'inc_path.php';
+require_once INCPATH.'header.php';
+navbar();
+
+// the unique contents will go here up to the footer
+?>
+<h3>The Egoscue Method</h3>
+<p>I have added another tool to my toolbox which is the Egoscue Method of postural alignment. Popular in the USA but fairly unheard of in the UK.</p>
+<p>Over the years I came to realize that although I was quite good at releasing the soft tissue of the body that if clients would or could not work on strengthening the muscles that had weakened and address postural problems, the (pain) symptoms would come back.</p>
+<p>I have also struggled with my own knee injury that was a result of a chronic imbalance so I looked for something that worked after not getting the results I hoped for with many other modalities. The Egoscue Method execises worked!</p>
+<p>I think that people like to feel empowered to help themselves when musculoskeletal issues strike and not be dependent only on a body therapist. Even better, with proper alignment of the body chronic pain will disappear and one can have a massage just for pleasure!</p>
+<p>The Egoscue Method is a great tool for self care and to me, the Egoscue method is the perfect active partner to Myofascial Release and Remedial massage.</p>
+<?php img_tag('egoscue1.jpg','','egoscue1'); ?>
+<?php img_tag('egoscue2.jpg','','egoscue2'); ?>
+<h3>How it works:</h3>
+<p>The first appointment will be the longest.</p>
+<p>You will have you photos taken, front, back and sides. (Please wear tight fitting possibly light coloured clothing or shorts and top). A plumb line will be superimposed on the photo and show you your vertical and horizontal misalignment or alignment.</p>
+<p>There is an intake form to fill in and a health history will be taken.</p>
+<p>Functional tests will be done to identify dysfunction and compensation.</p>
+<p>There will be a comparing the condition presented with the position of the body.</p>
+<p>Then you will have a personal menu of gentle exercises and stretches created for you addressing your personal body issues, which you will do usually daily for 1 week before you come back to have your menu revised, questions answered and checked if the exercises are properly done. Doing a menu can take anything from 15 min to 2 hrs. We will discuss how to fit it in your daily routine. It is a commitment both financially and timewise and you will be asked to set goals so progress can be measured.</p>
+<p>Apart from helping you to getting pain free, the Egoscue Method can also help you to do your favourite acivities more efficiently and  better!</p>
+<h3>Links and resources:</h3>
+<p>Website: <a href="http://www.egoscue.com/" target="_blank" title="visit the Egoscue website">www.egoscue.com</a></p>
+<p>YouTube: <a href="https://www.youtube.com/results?search_query=Egoscue+for+chronic+knee+pain" target="_blank">Egoscue for chronic knee pain</a><br>
+<a href="https://www.youtube.com/results?search_query=Egoscue+how+to+ease+lower+back+pain" target="_blank">Egoscue how to ease lower back pain</a><br>
+<a href="https://www.youtube.com/results?search_query=egoscue+how+to+fix+neck+and+shoulder+pain" target="_blank">Egoscue how to fix neck and shoulder pain</a><br>
+<a href="https://www.youtube.com/results?search_query=Egoscue+for+a+more+comfortable+flight" target="_blank">Egoscue for a more comfortable flight</a><br>
+<a href="https://www.youtube.com/results?search_query=Egoscue+how+to+ease+lower+back+pain" target="_blank">Egoscue how to ease lower back pain</a><br>
+<a href="https://www.youtube.com/results?search_query=Egoscue+alignment+exercises+to+do+during+a+long+drive" target="_blank">Egoscue alignment exercises to do during a long drive</a><br>
+<a href="https://www.youtube.com/results?search_query=Egoscue++for+the+office+" target="_blank">Egoscue  for the office </a><br>
+</p>
+<p>They are all great and give you a good idea but the most effective method is having your personal menu.</p>
+
+<?php footer(); ?>
